30a1f3b18b021e8cacbd8fe01516df5ac5d88165
banksinn <sintanawongsupakdee@gmail.com>
2022-12-16 02:58:30 +0700

    finish mart disneyland

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,21 @@ import { SessionProvider } from "next-auth/react"
 import "../styles/globals.css"
 import "../styles/ibm.css"
 import "antd/dist/reset.css"
+import dayjs from "dayjs"
+
+import advancedFormat from "dayjs/plugin/advancedFormat"
+import customParseFormat from "dayjs/plugin/customParseFormat"
+import localeData from "dayjs/plugin/localeData"
+import weekday from "dayjs/plugin/weekday"
+import weekOfYear from "dayjs/plugin/weekOfYear"
+import weekYear from "dayjs/plugin/weekYear"
+
+dayjs.extend(customParseFormat)
+dayjs.extend(advancedFormat)
+dayjs.extend(weekday)
+dayjs.extend(localeData)
+dayjs.extend(weekOfYear)
+dayjs.extend(weekYear)
 
 function MyApp({ Component, pageProps }) {
    const getLayout = Component.getLayout || ((page) => page)
