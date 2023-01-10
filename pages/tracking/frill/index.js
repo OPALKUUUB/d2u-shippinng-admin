31ac0a1dddfddf42cf788110cb094142dbf71960
banksinn <sintanawongsupakdee@gmail.com>
2023-01-10 19:26:23 +0700

    fix bug onPreview

--- pages/tracking/frill/index.js
+++ pages/tracking/frill/index.js
@@ -216,8 +216,7 @@ function FrillPage() {
             reader.onload = () => resolve(reader.result)
          })
       }
-      // eslint-disable-next-line no-restricted-globals
-      const image = new Image(screen.width)
+      const image = new Image(500)
       image.src = src
       const imgWindow = window.open(src)
       imgWindow?.document.write(image.outerHTML)
