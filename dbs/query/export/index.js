e99e0882bfc7a0ca46a9d8b81603942b5b14647c
Makara Atipatipat <makara.atipat@gmail.com>
2023-06-17 17:06:15 +0700

    [Enhanced] yen in data analysis

--- dbs/query/export/index.js
+++ dbs/query/export/index.js
@@ -5,14 +5,19 @@ import query from "../../mysql/connection"
 async function getTrackingExport(startDate, endDate) {
    const sql = `
         SELECT
-            t.date, t.channel, t.delivery_type, t.track_no, t.box_no, t.weight, t.price, t.voyage, t.remark_admin,
+            t.date, t.channel, t.delivery_type, t.track_no, t.box_no, t.weight, t.voyage, t.remark_admin,
+            CASE
+                WHEN t.channel = 'yahoo' THEN FORMAT(CEIL(yap.bid * yap.rate_yen), 0)
+                WHEN t.price IS NULL THEN 0
+                ELSE FORMAT(t.price, 0)
+            END AS price,
             u.username, u.name, u.phone
         FROM
             trackings t
         JOIN
-            users u
-        ON
-            u.id = t.user_id
+            users u ON u.id = t.user_id
+        LEFT JOIN
+            \`yahoo-auction-payment\` yap ON t.id = yap.tracking_id
         WHERE
             STR_TO_DATE(t.date, '%d/%m/%Y') BETWEEN STR_TO_DATE(?, '%Y-%m-%d') AND STR_TO_DATE(?, '%Y-%m-%d')
         ORDER BY 
