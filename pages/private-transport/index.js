830fd53345583b5e46a903158ed69ab7ba336bbd
Rattasat Sawatwongchai <d6600128@outsource.ktb.co.th>
2023-07-15 15:09:58 +0700

    [add] layout

--- pages/private-transport/index.js
+++ pages/private-transport/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Layout from "../../components/layout/layout"
+import { getSession } from 'next-auth/react'
 
-function index() {
+function PrivateTransport() {
    return (
       <div>
       test
@@ -8,4 +10,25 @@ function index() {
    )
 }
 
-export default index
+PrivateTransport.getLayout = function getLayout(page) {
+   return <Layout>{page}</Layout>
+}
+
+export async function getServerSideProps(context) {
+   const session = await getSession({ req: context.req })
+   if (!session) {
+      return {
+         redirect: {
+            destination: "/auth/signin",
+            permanent: false,
+         },
+      }
+   }
+   return {
+      props: {
+         session,
+      },
+   }
+}
+
+export default PrivateTransport
