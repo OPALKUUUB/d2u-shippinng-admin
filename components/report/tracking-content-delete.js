50daab63b41299c44421811f70a1968e55d52f04
OPALKUUUB <makara.atipat@gmail.com>
2023-10-28 01:47:27 +0700

    fix lint

--- components/report/tracking-content-delete.js
+++ components/report/tracking-content-delete.js
@@ -1,65 +1,65 @@
 import { Table } from "antd"
-import axios from "axios";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import axios from "axios"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
 function TrackingContentDelete() {
-    const [data, setData] = useState()
-    const [loading, setLoading] = useState(false)
-    const getDataTable = async () => {
-        setLoading(true)
-        try {
-            const url = `/api/tracking/contentDelete`
-            const response = await axios.get(url)
-            const responseData = response.data.data
-            setData(responseData)
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false)
-            console.log("Get Data Table Successfully!");
-        }
-    }
+   const [data, setData] = useState()
+   const [loading, setLoading] = useState(false)
+   const getDataTable = async () => {
+      setLoading(true)
+      try {
+         const url = `/api/tracking/contentDelete`
+         const response = await axios.get(url)
+         const responseData = response.data.data
+         setData(responseData)
+      } catch (error) {
+         console.log(error)
+      } finally {
+         setLoading(false)
+         console.log("Get Data Table Successfully!")
+      }
+   }
 
-    useEffect(() => {
-        getDataTable()
-    }, [])
+   useEffect(() => {
+      getDataTable()
+   }, [])
 
-    const columns = [
-        {
-            title: "วันที่",
-            dataIndex: "date",
-            key: "date"
-        },
-        {
-            title: "ชื่อลูกค้า",
-            dataIndex: "username",
-            key: "username",
-        },
-        {
-            title: "URL",
-            dataIndex: "link",
-            key: "link",
-            render: (text) => text === "" || text === null ? "-" : <Link href={text}>{text.slice(0, 10)}...</Link>
-        },
-        {
-            title: "หมายเหตุ",
-            dataIndex: "remark_admin",
-            key: "remark_admin",
-        },
-        {
-            title: "created_at",
-            dataIndex: "created_at",
-            key: "created_at",
-        },
-    ]
-    return (
-        <Table
-            columns={columns}
-            dataSource={data}
-            loading={loading}
-        />
-    )
+   const columns = [
+      {
+         title: "วันที่",
+         dataIndex: "date",
+         key: "date"
+      },
+      {
+         title: "ชื่อลูกค้า",
+         dataIndex: "username",
+         key: "username",
+      },
+      {
+         title: "URL",
+         dataIndex: "link",
+         key: "link",
+         render: (text) => text === "" || text === null ? "-" : <Link href={text}>{text.slice(0, 10)}...</Link>
+      },
+      {
+         title: "หมายเหตุ",
+         dataIndex: "remark_admin",
+         key: "remark_admin",
+      },
+      {
+         title: "created_at",
+         dataIndex: "created_at",
+         key: "created_at",
+      },
+   ]
+   return (
+      <Table
+         columns={columns}
+         dataSource={data}
+         loading={loading}
+      />
+   )
 }
 
 export default TrackingContentDelete
--- pages/tracking/contentDelete/index.js
+++ pages/tracking/contentDelete/index.js
@@ -4,33 +4,33 @@ import TrackingContentDelete from "../../../components/report/tracking-content-d
 import CardHead from "../../../components/CardHead"
 
 function ContentDeleteTracking() {
-    return (
-        <div className="w-full">
-            <CardHead name="ประวัติการลบรายการ" description="แสดงรายการที่ถูกลบใน mercari, fril, shimizu, web123 (ตอนนี้ยังไม่สามารถเก็บประวัติของ yahoo ได้)" />
-            <div className="bg-white px-4 py-3 m-2">
-                <TrackingContentDelete />
-            </div>
-        </div>
-    )
+   return (
+      <div className="w-full">
+         <CardHead name="ประวัติการลบรายการ" description="แสดงรายการที่ถูกลบใน mercari, fril, shimizu, web123 (ตอนนี้ยังไม่สามารถเก็บประวัติของ yahoo ได้)" />
+         <div className="bg-white px-4 py-3 m-2">
+            <TrackingContentDelete />
+         </div>
+      </div>
+   )
 }
 
 ContentDeleteTracking.getLayout = function getLayout(page) {
-    return <Layout>{page}</Layout>
+   return <Layout>{page}</Layout>
 }
 
 export async function getServerSideProps(context) {
-    const session = await getSession({ req: context.req })
+   const session = await getSession({ req: context.req })
 
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/auth/signin",
-                permanent: false,
-            },
-        }
-    }
-    return {
-        props: { session },
-    }
+   if (!session) {
+      return {
+         redirect: {
+            destination: "/auth/signin",
+            permanent: false,
+         },
+      }
+   }
+   return {
+      props: { session },
+   }
 }
-export default ContentDeleteTracking;
+export default ContentDeleteTracking
