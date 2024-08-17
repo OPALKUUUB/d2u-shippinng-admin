4e9989192bfb15332108ecc7f361a2469a0b6fc3
OPALKUUUBDEV <makara.atipatipat.dev@gmail.com>
2024-08-17 10:13:27 +0700

    fix: cors

---
+++ middleware.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server"
+
+const allowedOrigins = [
+   "http://localhost:5173",
+   "https://web-invoice.d2u-shipping.com",
+]
+
+const corsOptions = {
+   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
+export function middleware(request) {
+   const origin = request.headers.get("origin") ?? ""
+   const isAllowedOrigin = allowedOrigins.includes(origin)
+   let response = NextResponse.next()
+   if (isAllowedOrigin) {
+      console.log("Allowed Origin", origin)
+
+      response.headers.set("Access-Control-Allow-Origin", origin)
+   }
+   Object.entries(corsOptions).forEach(([key, value]) => {
+      response.headers.set(key, value)
+   })
+   console.log(request);
+   
+   return response
+}
+
+export const config = {
+   matcher: "/api/:path*",
+}
